80980016846fcc81da9a34f2095e24a83d04bb92
harry <546462977@qq.com>
2025-05-26 21:00:51 +0800

    修改dataset拆分

--- utils/sft_dataset.py
+++ utils/sft_dataset.py
@@ -8,29 +8,31 @@ class SFTJsonlDataset(Dataset):
         self.samples = []
         self.tokenizer = tokenizer
         self.seq_len = seq_len
+        half_seq_len = seq_len // 2
         with open(jsonl_file, 'r', encoding='utf-8') as f:
             for line in f:
                 j = json.loads(line)
                 instruction = j.get("instruction", "")
-                input_ = j.get("input", "")
                 output = j.get("output", "")
                 # 这里可根据你的prompt模板格式调整 ↓↓↓
                 # 例：Alpaca
-                if input_:
-                    prompt = f"Instruction: {instruction}\nInput: {input_}\nOutput:"
-                else:
-                    prompt = f"Instruction: {instruction}\nOutput:"
+                prompt = f"Instruction: {instruction}\nOutput:"
                 prompt_token_ids = tokenizer.encode(prompt)
                 output_token_ids = tokenizer.encode(output)
-                input_ids = prompt_token_ids + output_token_ids
-                labels = [-100] * len(prompt_token_ids) + output_token_ids
-                input_ids = input_ids[:seq_len]
-                labels = labels[:seq_len]
-                if len(input_ids) < seq_len:
-                    input_ids += [PAD_TOKEN_ID] * (seq_len - len(input_ids))
-                    labels += [-100] * (seq_len - len(labels))
-                # target: output部分作为监督目标，可以根据需求mask prompt部分，只监督 output
-                # 简单做法：全部训练
+                if len(prompt_token_ids) < half_seq_len:
+                    input_ids = (prompt_token_ids + output_token_ids)[:seq_len]
+                    labels = [-100] * len(prompt_token_ids) + output_token_ids
+                    if len(input_ids) < seq_len:
+                        input_ids += [PAD_TOKEN_ID] * (seq_len - len(input_ids))
+                        labels += [-100] * (seq_len - len(labels))
+                else:
+                    new_prompt_token_ids = prompt_token_ids[:half_seq_len]
+                    input_ids = new_prompt_token_ids + output_token_ids
+                    labels = [-100] * len(new_prompt_token_ids) + output_token_ids
+                    input_ids = input_ids[:seq_len]
+                    if len(input_ids) < seq_len:
+                        input_ids += [PAD_TOKEN_ID] * (seq_len - len(input_ids))
+                        labels += [-100] * (seq_len - len(labels))
                 self.samples.append((input_ids, labels))
                 
     def __len__(self):
